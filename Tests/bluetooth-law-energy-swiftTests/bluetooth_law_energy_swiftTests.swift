b38de86af5b209f9720dcce208bf239385831e51
Igor <igorshelopaev@gmail.com>
2024-07-10 12:42:01 +0200

    Initial Commit

---
+++ Tests/bluetooth-law-energy-swiftTests/bluetooth_law_energy_swiftTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import bluetooth_law_energy_swift
+
+final class bluetooth_law_energy_swiftTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
