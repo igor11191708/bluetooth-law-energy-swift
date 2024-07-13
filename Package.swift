365762b6b9a27bb79722772de13c79d000825ed9
Igor <igorshelopaev@gmail.com>
2024-07-13 10:20:52 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "bluetooth-law-energy-swift",
-    platforms: [.macOS(.v11), .iOS(.v14)],
+    platforms: [.macOS(.v11), .iOS(.v14), .watchOS(.v8), .tvOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
