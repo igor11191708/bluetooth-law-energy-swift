3e2f7a0aa752a5c7cbe65e0f1028d9b33ed57190
Igor <igorshelopaev@gmail.com>
2024-07-20 08:27:13 +0200

    Update CBPeripheral+.swift

--- Sources/bluetooth-law-energy-swift/ext/CBPeripheral+.swift
+++ Sources/bluetooth-law-energy-swift/ext/CBPeripheral+.swift
@@ -7,21 +7,41 @@
 
 import CoreBluetooth
 
-public extension CBPeripheral{
+public extension CBPeripheral {
     
-    var isConnected : Bool{
+    // MARK: - Computed Properties
+    
+    /// Checks if the peripheral is connected.
+    var isConnected: Bool {
         self.state == .connected
     }
     
-    var isNotConnected : Bool{
+    /// Checks if the peripheral is not connected.
+    var isNotConnected: Bool {
         self.state != .connected
     }
     
+    /// Retrieves the identifier of the peripheral.
     var getId: UUID {
         return self.identifier
     }
     
-    var getName : String{
+    /// Retrieves the name of the peripheral. If the name is nil, returns "unknown".
+    var getName: String {
         name ?? "unknown"
     }
+}
+
+
+fileprivate func censorWords(in text: String, using words: [String]) -> String {
+    var censoredText = text
+    
+    for word in words {
+        let pattern = "\\b\(word)\\b"
+        let regex = try! NSRegularExpression(pattern: pattern, options: .caseInsensitive)
+        let range = NSRange(location: 0, length: censoredText.utf16.count)
+        censoredText = regex.stringByReplacingMatches(in: censoredText, options: [], range: range, withTemplate: String(repeating: "*", count: word.count))
+    }
+    
+    return censoredText
 }
