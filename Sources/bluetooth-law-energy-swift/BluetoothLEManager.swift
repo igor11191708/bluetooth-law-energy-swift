017fe3ee8c7a867f1f794135a6f7908f6501c8b7
Igor <igorshelopaev@gmail.com>
2024-07-23 17:11:03 +0200

    Update BluetoothLEManager.swift

--- Sources/bluetooth-law-energy-swift/BluetoothLEManager.swift
+++ Sources/bluetooth-law-energy-swift/BluetoothLEManager.swift
@@ -38,7 +38,7 @@ public actor BluetoothLEManager: NSObject, ObservableObject, IBluetoothLEManager
     private let stream : StreamFactory
     private let delegateHandler: Delegate
     private var cancellables: Set<AnyCancellable> = []
-    private let retry = RetryService(strategy: .exponential(retry: 3, multiplier: 2, duration: .seconds(3), timeout: .seconds(12)))
+    private let retry = RetryService(strategy: .exponential(retry: 5, multiplier: 2, duration: .seconds(3), timeout: .seconds(15)))
     private let logger: ILogger
     
     /// Initializes the BluetoothLEManager with a logger.
