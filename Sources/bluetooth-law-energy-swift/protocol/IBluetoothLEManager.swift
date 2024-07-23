e2a09669870b908a833b74a793e9e3e87de38b01
Igor <igorshelopaev@gmail.com>
2024-07-23 15:44:50 +0200

    readme

--- Sources/bluetooth-law-energy-swift/protocol/IBluetoothLEManager.swift
+++ Sources/bluetooth-law-energy-swift/protocol/IBluetoothLEManager.swift
@@ -11,14 +11,14 @@ import CoreBluetooth
 
 @available(macOS 12, iOS 15, tvOS 15.0, watchOS 8.0, *)
 /// A protocol defining the Bluetooth LE manager functionality.
+@MainActor
 public protocol IBluetoothLEManager {
 
     /// A subject that publishes the BLE state changes.
-    @MainActor
+    
     var bleState: CurrentValueSubject<BLEState, Never> { get }
 
     /// Provides an asynchronous stream of discovered Bluetooth peripherals.
-    @MainActor
     var peripheralsStream: AsyncStream<[CBPeripheral]>  { get async }
 
     /// Discovers services for a given peripheral, with optional caching and optional disconnection.
@@ -28,20 +28,17 @@ public protocol IBluetoothLEManager {
     ///   - disconnect: A Boolean value indicating whether to disconnect from the peripheral after fetching services. Defaults to `true`.
     /// - Returns: An array of `CBService` instances.
     /// - Throws: An error if the services could not be fetched.
-    @MainActor
     func discoverServices(for peripheral: CBPeripheral, from cache: Bool, disconnect: Bool) async throws -> [CBService]
 
     /// Connects to a specific peripheral.
     /// Always use the same CBCentralManager instance to manage connections and disconnections for a peripheral to avoid errors and ensure correct behavior.
     /// - Parameter peripheral: The `CBPeripheral` instance to connect to.
     /// - Throws: `BluetoothLEManager.Errors` if the connection fails.
-    @MainActor
     func connect(to peripheral: CBPeripheral) async throws
 
     /// Disconnects from a specific peripheral.
     /// Always use the same CBCentralManager instance to manage connections and disconnections for a peripheral to avoid errors and ensure correct behavior.
     /// - Parameter peripheral: The `CBPeripheral` instance to disconnect from.
     /// - Throws: `BluetoothLEManager.Errors` if the disconnection fails.
-    @MainActor
     func disconnect(from peripheral: CBPeripheral) async throws
 }
